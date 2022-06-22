4e21cc99e6de62992fb3371c290e86ab4a419085
AlessP93 <alespiscino@libero.it>
2022-06-22 17:38:01 +0200

    fix

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
     $lastname = 'Piscino';
     $text = str_replace('Alessandro', '***', $name);
     $list = ['pane', 'latte', 'uova'];
-
+    // creo un array vuoto
     $array = [];
-
+    //la lunghezza dell array e di 15 volte
     while (count($array) < 15) {
+        //genero un numero random da 1 a 100
         $num_rand = rand(1, 100);
-
+        // se il num rand e dentro l array (duplicato)  IN_ARRAY
         if(!in_array($num_rand, $array)) {
             $array[] = $num_rand;
         }
@@ -25,9 +26,16 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Ciao a tutti mi chiamo <?php echo $name; ?> <?php echo $lastname; ?></h1>
-    <h3>Lunghezza testo: <?php echo strlen($name); ?></h3>
-    <h1> <?php echo $text; ?> </h1>
-    <h3> <?php var_dump($list); ?></h3>
+    <div>
+        <h1>Ciao a tutti mi chiamo <?php echo $name; ?> <?php echo $lastname; ?></h1>
+        <h3>Lunghezza testo: <?php echo strlen($name); ?></h3>
+        <h1> <?php echo $text; ?> </h1>
+        <h3> <?php var_dump($list); ?></h3>
+    </div>
+    <div>
+        <?php // se stampiamo con un array va il var_dump invece echo per testo e numeri ( variabili )
+            var_dump($array)
+        ?>
+    </div>
 </body>
 </html>
